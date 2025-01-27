3839d5cad984e0ed4bf85df3228cf63291b8ce9a
Infezt <99657022+Infezt@users.noreply.github.com>
2025-01-27 20:00:09 +0300

    Create dotnet-install.ps1

---
+++ dotnet-install.ps1
@@ -0,0 +1,9 @@
+dotnet-install.ps1 [-Architecture <ARCHITECTURE>] [-AzureFeed]
+    [-Channel <CHANNEL>] [-DryRun] [-FeedCredential]
+    [-InstallDir <DIRECTORY>] [-JSonFile <JSONFILE>]
+    [-NoCdn] [-NoPath] [-ProxyAddress] [-ProxyBypassList <LIST_OF_URLS>]
+    [-ProxyUseDefaultCredentials] [-Quality <QUALITY>] [-Runtime <RUNTIME>]
+    [-SkipNonVersionedFiles] [-UncachedFeed] [-Verbose]
+    [-Version <VERSION>]
+
+Get-Help ./dotnet-install.ps1
